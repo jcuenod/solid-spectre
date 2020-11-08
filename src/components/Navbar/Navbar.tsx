af54543890aa7e057562dfab657bdff6ff2de9c1
James Cuénod <j3frea+gitkraken@gmail.com>
2020-11-08 12:31:49 -0600

    Don't use props, pass through style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,25 +4,30 @@ export type INavbarProps = {
 	left?: JSX.Element,
 	center?: JSX.Element,
 	right?: JSX.Element
+	style: string
 }
 
-const Navbar: Component<INavbarProps> = ({ left, center, right }) =>
-	<header class="navbar">
-		{left || center || right ?
-			<section class="navbar-section">
-				{left || null}
-			</section> : null}
+const Navbar: Component<INavbarProps> = (props) => {
+	const { left, center, right, style } = props
+	return (
+		<header class="navbar" style={style}>
+			{left || center || right ?
+				<section class="navbar-section">
+					{left || null}
+				</section> : null}
 
-		{center ?
-			<section class="navbar-center">
-				{center}
-			</section> : null}
+			{center ?
+				<section class="navbar-center">
+					{center}
+				</section> : null}
 
-		{right || center ?
-			<section class="navbar-section">
-				{right || null}
-			</section> : null}
-	</header >
+			{right || center ?
+				<section class="navbar-section">
+					{right || null}
+				</section> : null}
+		</header>
+	)
+}
 
 
 export default Navbar
